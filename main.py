391b9b0c3ae2a546875eff30f59a8cfbb88cf280
BEARaldo <95887800+BEARaldo@users.noreply.github.com>
2023-05-17 16:48:07 -0300

    Add files via upload
    Changed from MySQL to Deta.
    Minor improvements to perform searches and deletions in the wishlist

--- main.py
+++ main.py
@@ -1,153 +1,181 @@
-from typing import Union, Optional
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-import mysql.connector
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
+from deta import Deta
 
-def send_email(to_email: str, wishlist_id: int, server_url: str):
-    try:
-        # Gerar URL da lista de desejos
-        wishlist_url = f"{server_url}/wishlist?id={wishlist_id}"
-
-        # Configurar mensagem de e-mail
-        message = MIMEMultipart()
-        message["Subject"] = "Lista de Desejos"
-        message["From"] = "geraldopjc@gmail.com"
-        message["To"] = to_email
-        text = f"Olá, segue o link da sua lista de desejos: {wishlist_url}"
-        message.attach(MIMEText(text, "plain"))
-
-        # Configurar servidor SMTP
-        server = smtplib.SMTP("smtp.gmail.com", 587)
-        server.starttls()
-        server.login("geraldopjc@gmail.com", "30125047GGdf")
-
-        # Enviar e-mail
-        server.sendmail("geraldopjc@gmail.com", to_email, message.as_string())
-        server.quit()
-
-        return {"detail": f"E-mail enviado com sucesso para {to_email}"}
-
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Erro ao enviar e-mail: {e}")
-
-cnx = mysql.connector.connect(user='root', database='ceublivre')
 app = FastAPI()
 
+deta = Deta('e05tW9KfFXAh_1X6VitLLEkY14CfB4eNXwHWStznACk91')
+dbUsers = deta.Base("Users") #Base temp. de usuários
+dbProducts = deta.Base("Products") #Base de produtos ativos temp.
+dbWishlist = deta.Base("wishlists") #base de wishlists(?)
+
+drive = deta.Drive("Images") # Drive de Imagens (ativos)
+dbInactiveProducts = deta.Base("ProductDatabase")# Base de produtos(Inativos)
+
+
 class Users(BaseModel):
-    id: int | None
+    key: str | None
     name: str
     mail: str
 
+
 class Product(BaseModel):
-    id: int | None
+    key: str | None
     name: str
     description: str
     category: str
     price: float
 
-class wishlist(BaseModel):
-    id: int | None
-    cod_user: int
-    cod_product: int
+class Wishlist(BaseModel):
+    key: str | None
+    cod_user: str
+    cod_product: str
 
-def get_id_wishlist(iduser):
-    cursor = cnx.cursor(dictionary=True)
-    select_statement = "SELECT user_name, GROUP_CONCAT(product_name) AS holder "\
-                        "FROM products JOIN wishlist ON product_id=cod_product "\
-                        "JOIN users ON user_id=cod_user "\
-                        "WHERE user_id = %s "\
-                        "GROUP BY user_name"
-    val = (iduser,)
-    cursor.execute(select_statement, val)
-    result = cursor.fetchall()
-    return result
+
 
 
 @app.get("/users")
 async def get_users():
-    cursor = cnx.cursor(dictionary=True)
-    query = 'SELECT * FROM users'
-    cursor.execute(query)
+    result = dbUsers.fetch()
+    return result
 
-    return cursor.fetchall()
-
-@app.get("/userwishlist")
-async def get_user_wishlist(iduser: int):
-    result = get_id_wishlist(iduser)
-    if not result:
-        raise HTTPException(status_code=404, detail="Nenhum produto encontrado na lista de desejos.")
+@app.post("/users")
+async def register_user(user: Users):
+    new_item = {
+        "name": user.name,
+        "mail": user.mail
+    }
+    result = dbUsers.insert(new_item)
+    if result.get("failed"):
+        raise HTTPException(status_code=404, detail="Falhao ao inserir, olha essa porra ai.")
     else:
-        return result
-
-@app.post("/wishlistpost")
-async def insert_product_wishlist(iduser: int, idproduct: int):
-    cursor = cnx.cursor(dictionary=True)
-    statement = "INSERT INTO wishlist(cod_product, cod_user)" \
-                "VALUES (%s, %s)"
-    val = (idproduct, iduser)
-    #statement = "UPDATE products SET product_name = %s, product_description = %s, product_category = %s, product_price = %s "\
-     #               "WHERE product_id = %s"
+        return {"detail": "Adicionado a lista de usuários com sucesso."}
 
 
 
-    cursor.execute(statement, val)
-    cnx.commit()
+@app.get("/products")
+async def get_product():
+    result = dbProducts.fetch().items
+    return result
 
-    raise HTTPException(status_code=500, detail="Produto não encontrado ou ja na lista de desejo.")
+@app.post("/products")
+async def insert_product(product: Product):
+    new_item = {
+        "name": product.name,
+        "description": product.description,
+        "category": product.category,
+        "price": product.price
+    }
+    result = dbProducts.insert(new_item)
+    if result.get("failed"):
+        raise HTTPException(status_code=404, detail="Falhao ao inserir, olha essa porra ai.")
+    else:
+        return {"detail": "Adicionado a lista de produtos com sucesso."}
 
-    return {"detail": "Adicionado a lista de desejo com sucesso."}
+
+###################################################################################################################################################################################################################################################################
+###################################################################################################################################################################################################################################################################
+
+@app.get("/wishlist/{id}")
+async def get_wishlist_by_id(id: str):
+    wishlist_items = dbWishlist.fetch({"cod_user": id}).items
+    if not wishlist_items:
+        raise HTTPException(status_code=404, detail="Nenhum produto encontrado na lista de desejos do usuário.")
+    keys = [item["cod_product"] for item in wishlist_items]
+    products = []
+    for key in keys:
+        product = dbProducts.get(key)
+        if product:
+            product_info = {
+                "nome": product["name"],
+                "descrição": product["description"],
+                "categoria": product["category"],
+                "preço": product["price"]
+            }
+            products.append(product_info)
+
+    if not products:
+        raise HTTPException(status_code=404, detail="Nenhum produto encontrado na wishlist.")
+
+    return products
 
 
 
-@app.delete("/wishlistdel")
-async def delete_product_wishlist(wish_id: int):
+
+
+@app.get("/wishlist")
+async def get_user_wishlist(user_name: str):
+    user = dbUsers.fetch({"name": user_name}).items
+    if not user:
+        raise HTTPException(status_code=404, detail="Usuário não encontrado.")
+
+    wishlist_items = dbWishlist.fetch({"cod_user": user[0]["key"]}).items
+    if not wishlist_items:
+        raise HTTPException(status_code=404, detail="Nenhum produto encontrado na lista de desejos do usuário.")
+
+    product_keys = [item["cod_product"] for item in wishlist_items]
+
+ # Consulta para obter os nomes dos produtos com base nas chaves - erroddd
+    products = []
+    for key in product_keys:
+        product = dbProducts.get(key)
+        if product:
+            product_info ={
+                "nome": product["name"],
+                "descrição": product["description"],
+                "categoria" : product["category"],
+                "preço": product["price"]
+            }
+            products.append(product_info)
+    if not products:
+        raise HTTPException(status_code=404, detail="Nenhum produto encontrado na wishlist.")
+
+
+    return products
+
+
+
+
+@app.post("/wishlist")
+async def insert_product_wishlist(iduser: str, idproduct: str):
+    new_item = {
+        "cod_product": idproduct,
+        "cod_user": iduser
+    }
+    result = dbWishlist.insert(new_item)
+    if result.get("failed"):
+        raise HTTPException(status_code=500, detail="Produto não encontrado ou já na lista de desejos.")
+    else:
+        return {"detail": "Adicionado a lista de desejo com sucesso."}
+
+@app.delete("/wishlist")
+async def delete_product_wishlist_by_wid(wish_id: str):
     try:
-        cursor = cnx.cursor(dictionary=True)
-        select_statement = "SELECT * FROM wishlist WHERE id_wishlist = %s"
-        val = (wish_id,)
-        cursor.execute(select_statement, val)
-        result = cursor.fetchone()
+        result = dbWishlist.get(wish_id)
         if not result:
-            raise HTTPException(status_code=404, detail="Produto não encontrado na lista de desejos.")
+            return {"detail": "Código de Produto não encontrado na lista de desejos."}
+        if result:
+            dbWishlist.delete(wish_id)
+            return {"detail": "Código de Produto excluído com sucesso da lista de desejos."}
         else:
-            delete_statement = "DELETE FROM wishlist WHERE id_wishlist = %s"
-            cursor.execute(delete_statement, val)
-            cnx.commit()
-            return {"detail": "Produto excluído com sucesso da lista de desejos."}
-    except mysql.connector.Error as err:
-        raise HTTPException(status_code=500, detail=f"Erro ao excluir produto da lista de desejos: {err.msg}")
+            raise HTTPException(status_code=404, detail=f"Erro ao excluir produto da lista de desejos: {str(e)}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Erro ao excluir produto da lista de desejos: {str(e)}")
 
-from typing import Optional
+
 
 @app.post("/wishlistshare")
-async def share_wishlist(email: str, wish_id: int, subject: Optional[str] = "Lista de Desejos Compartilhada"):
+async def share_wishlist(email: str, wish_id: str):
     try:
-        cursor = cnx.cursor(dictionary=True)
-        select_user_statement = "SELECT * FROM users WHERE user_mail = %s"
-        val = (email,)
-        cursor.execute(select_user_statement, val)
-        user_result = cursor.fetchone()
-        if not user_result:
+        select_user = db.query({"mail": email}).fetch()
+        if not select_user:
             raise HTTPException(status_code=404, detail="Usuário com o e-mail fornecido não encontrado.")
         else:
-            if 'user_id' in user_result:
-                update_statement = "UPDATE wishlist SET cod_user = %s WHERE id_wishlist = %s"
-                val = (user_result['user_id'], wish_id)
-                cursor.execute(update_statement, val)
-                cnx.commit()
-                # chamando a função send_email para enviar o e-mail
-                wishlist_items = get_id_wishlist(user_result['user_id'])
-                if wishlist_items:
-                    products = ", ".join([item['holder'] for item in wishlist_items])
-                else:
-                    products = "Nenhum produto encontrado na lista de desejos."
-                message = f"Olá, segue abaixo o link para acessar a sua lista de desejos: http://localhost/userwishlist?iduser={user_result['user_id']}\nProdutos: {products}"
-                send_email(email, subject, message)
-                return {"detail": f"Lista de desejos compartilhada com sucesso com o e-mail {email}"}
+            user_id = select_user[0]["id"]
+            update_result = db.update({"cod_user": user_id}, {"$push": {"wishlist": wish_id}})
+            if update_result.get("failed"):
+                raise HTTPException(status_code=500, detail="Erro ao compartilhar lista de desejos.")
             else:
-                raise HTTPException(status_code=500, detail="Campo 'id' não encontrado no resultado da consulta.")
-    except mysql.connector.Error as err:
-        raise HTTPException(status_code=500, detail=f"Erro ao compartilhar lista de desejos: {err.msg}")
+                return {"detail": f"Lista de desejos compartilhada com sucesso com o e-mail {email}"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Erro ao compartilhar lista de desejos: {str(e)}")
